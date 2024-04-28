6ddc3b91ef13c51c9dd7189b8f9bf7fab6ea9848
chpolyzo <49782010+chpolyzo@users.noreply.github.com>
2024-04-28 18:43:37 +0200

    working around OOP in python

--- calcolare_votanti_oop.py
+++ calcolare_votanti_oop.py
@@ -8,105 +8,75 @@ def clearConsole():
     command = "clear"
     os.system(command)
 
-def nuova_operazione():
-    """
-    Funzione che ha il solo scopo di
-    1. visualizzare le varie voci
-    2. ricevere l'input dall'utente
-    input: NIENTE
-    output: la selezione dell'utente
-    """
-    clearConsole()
-    print("Ecco cosa puoi fare:")
-    print("1 - Nuova votazione")
-    print("2 - Vedi lista votanti")
-    print("3 - Risultato votazioni")
-    print("4 - Vincitore")
-    print("q - Esci")
-    carattere = input(">>> Inserisci un comando:")
-    clearConsole()
-    return carattere
+
+'''
+We define a class called Election.
+'''
 
 
-class Persona(object):
+class Election:
+    '''
+    The __init__ method initializes the attributes representing the number of registered voters, total voters, yes votes, and no votes.
+    '''
 
-    # constructor of the class
-    def __init__(self, nome='Privacy please'):
-        self.name = nome
-        self.voto = ''
+    def __init__(self):
+        self.registered_voters = 0
+        self.total_voters = 0
+        self.yes_votes = 0
+        self.no_votes = 0
 
-    def vota(self, preferenza):
-        self.voto = preferenza
+    '''
+    The input_results method prompts the user to input the relevant data.
+    include a validation step within the input_results method to ensure that the number of voters 
+    is not more than the number of registered voters.
+    After taking input for the number of voters (self.total_voters), it enters a while loop.
+    Inside the loop, it checks if the number of voters (self.total_voters) 
+    is less than or equal to the number of registered voters (self.registered_voters).
+    If the condition is met (i.e., the number of voters is not more than the number of registered voters), the loop breaks.
+    If the condition is not met (i.e., the number of voters is more than the number of registered voters), 
+    it displays an error message and prompts the user to enter the number of voters again until a valid input is provided.
+    '''
 
-    def get_name(self):
-        return self.name
+    def input_results(self):
+        self.registered_voters = int(input("Enter the number of registered voters: "))
+        while True:
+            self.total_voters = int(input("Enter the number of voters: "))
+            if self.total_voters <= self.registered_voters:
+                break
+            else:
+                print("Error: Number of voters cannot exceed the number of registered voters.")
+        self.yes_votes = int(input("Enter the number of 'Yes' votes: "))
+        self.no_votes = int(input("Enter the number of 'No' votes: "))
 
-    def get_voto(self):
-        return self.voto
+    '''
+    The calculate_voter_percentage, calculate_yes_percentage, and calculate_no_percentage methods calculate the respective percentages.
+    '''
 
+    def calculate_voter_percentage(self):
+        return (self.total_voters / self.registered_voters) * 100
 
-def inserisci_scelta():
-    print("Benvenuto in cabina elettorale!")
-    print("Ecco la lista dei candidati")
-    for candidato in candidati:
-        print(candidato)
-    return input("Chi vuoi votare?")
+    def calculate_yes_percentage(self):
+        return (self.yes_votes / self.total_voters) * 100
 
+    def calculate_no_percentage(self):
+        return (self.no_votes / self.total_voters) * 100
 
-def calcola_statistiche(lista):
-    voti_validi = 0
-    voti_nulli = 0
-    astenuti = 0
-    for votante in lista:
-        if votante.get_voto() == "":
-            astenuti += 1
-        elif votante.get_voto() in candidati:
-            voti_validi +=1
-        else:
-            astenuti += 1
-    return [voti_validi, voti_nulli, astenuti]
+    '''
+    The display_results method prints the calculated percentages.
+    '''
 
-
-def totale_voti_candidato(candidato, lista):
-    voti_candidato = 0
-    for persona in lista:
-        if persona.get_voto() == candidato:
-            voti_candidato += 1
-    return voti_candidato
+    def display_results(self):
+        print("Percentage of voters out of total registered:", self.calculate_voter_percentage(), "%")
+        print("Percentage of 'Yes' votes:", self.calculate_yes_percentage(), "%")
+        print("Percentage of 'No' votes:", self.calculate_no_percentage(), "%")
 
 
-def nomina_vincitore(lista):
-    for candidato in candidati:
-        voti_candidato = totale_voti_candidato(candidato, lista)
-        print("Il candidato "+str(candidato)+ " ha riportato "+str(voti_candidato)+" voti.")
+'''
+In the main program, we create an instance of the Referendum class, input the results, and display the calculated percentages.
+'''
 
-
-comando = nuova_operazione()
-lista_votanti = []
-while comando != 'q':
-    if comando == "1":
-        nome_persona = input("chi sta votando? [invio per anonimato]")
-        if len(nome_persona)>0:
-            nuovo_oggetto_persona = Persona(nome_persona)
-        else:
-            nuovo_oggetto_persona = Persona()
-        scelta_personale = inserisci_scelta()
-        nuovo_oggetto_persona.vota(scelta_personale)
-        lista_votanti.append(nuovo_oggetto_persona)
-    if comando == "2":
-        print("Numero votanti: "+str(len(lista_votanti)))
-        for persona in lista_votanti:
-            print("---> "+str(persona.get_name()))
-    if comando == "3":
-        [voti_validi, voti_nulli, astenuti] = calcola_statistiche(lista_votanti)
-        print("Numero registrati: "+str(len(lista_votanti)))
-        print("Numero voti validi: "+str(voti_validi))
-        print("Numero voti nulli: "+str(voti_nulli))
-        print("Numero astenuti: " + str(astenuti))
-    if comando == "4":
-        vincitore = nomina_vincitore(lista_votanti)
-        print("Vincitore: " + str(vincitore))
-    # la comando non è tra quelle previste
-    print("Attenzione, inserisci un comando valido")
-    comando = nuova_operazione()
-print("Arrivederci")
+# Main program
+if __name__ == "__main__":
+    referendum = Election()
+    referendum.input_results()
+    referendum.display_results()
